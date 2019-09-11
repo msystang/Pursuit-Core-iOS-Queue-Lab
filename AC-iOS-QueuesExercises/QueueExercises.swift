bb1f9e18a64876af52e3398067b8b8c5bf788b60
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2019-09-11 17:31:30 -0400

    adds completed functions

--- AC-iOS-QueuesExercises/QueueExercises.swift
+++ AC-iOS-QueuesExercises/QueueExercises.swift
@@ -3,13 +3,37 @@ import Foundation
 //1. Find the smallest element in a queue
 
 func smallestElement<T: Comparable>(in q: Queue<T>) -> T? {
-    return nil
+    var myQueue = q
+    var smallest = myQueue.peek()
+    
+    guard !myQueue.isEmpty else { return nil }
+    
+    while !myQueue.isEmpty {
+        if let current = myQueue.dequeue() {
+            if let smallestUnwrapped = smallest {
+                if current < smallestUnwrapped {
+                    smallest = current
+                }
+            }
+        }
+    }
+    return smallest
 }
 
 //2. Find the sum of a queue of Ints
 
 func sum(of q: Queue<Int>) -> Int {
-    return 0
+    var myQueue = q
+    var sum = 0
+    
+    guard !myQueue.isEmpty else { return 0 }
+    
+    while !myQueue.isEmpty {
+        if let current = myQueue.dequeue() {
+            sum += current
+        }
+    }
+    return sum
 }
 
 //3. Find out whether or not a queue is in sorted order from smallest to biggest
@@ -20,8 +44,21 @@ False example   (Back) 4 - 19 - 134 - 200 (Front)
 */
 
 func isSorted<T: Comparable>(q: Queue<T>) -> Bool {
+    guard !q.isEmpty else { return true }
+    
+    var myQueue = q
+    var smallest = myQueue.dequeue()!
+    
+    while !myQueue.isEmpty {
+        let current = myQueue.dequeue()!
+        if current < smallest {
+            return false
+        }
+        smallest = current
+    }
     return true
 }
+
 
 //4. Given a Queue as input, return a reversed queue. (Hint: A stack can be helpful here)
 
@@ -31,12 +68,34 @@ Sample Output:  (Back) 31 - 2 - 16 - 9 (Front)
  */
 
 func reversed<T>(q: Queue<T>) -> Queue<T> {
-    return q
+    guard !q.isEmpty else { return q }
+    
+    var myQueue = q
+    var revQueue = Queue<T>()
+    var array = Array<T>()
+    
+    while !myQueue.isEmpty {
+        array.append(myQueue.dequeue()!)
+    }
+    
+    while !array.isEmpty {
+        revQueue.enqueue(array.popLast()!)
+    }
+    
+    return revQueue
 }
 
 //5 Determine if two Queues are equal
 
 func areEqual<T: Equatable>(qOne: Queue<T>, qTwo: Queue<T>) -> Bool {
+    var queueOne = qOne
+    var queueTwo = qTwo
+    
+    while !queueOne.isEmpty && !queueTwo.isEmpty {
+        if queueOne.dequeue() == queueTwo.dequeue() {
+            return true
+        }
+    }
     return false
 }
 
